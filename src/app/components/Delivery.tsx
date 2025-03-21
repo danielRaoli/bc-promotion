01ea85bbc43b964470ac6e7c8def2ae591e7c7b9
danielRaoli <danielraoli074@gmail.com>
2025-03-20 23:31:32 -0300

    feat: update delivery section

--- src/app/components/Delivery.tsx
+++ src/app/components/Delivery.tsx
@@ -1,9 +1,9 @@
 export function Delivery() {
   return (
     <>
-      <section className="w-full py-12 md:py-24 bg-black text-center">
+      <section className="w-full py-12 md:py-24 bg-black text-center px-4">
         <p className="text-blue-500 text-lg mb-4">Simples, rápido e prático</p>
-        <p className="text-white text-4xl max-w-[40%] mx-auto md:text-5xl">
+        <p className="text-white text-4xl md:max-w-[40%] w-full mx-auto md:text-5xl">
           Entregamos sua Landing Page em 5 dias úteis, pronta para converter
         </p>
       </section>
