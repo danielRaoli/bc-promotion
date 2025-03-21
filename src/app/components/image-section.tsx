f9d0b82d354c0c1bdaf5ce972744f74c128c5a08
danielRaoli <danielraoli074@gmail.com>
2025-03-20 23:03:51 -0300

    feat: change image section

--- src/app/components/image-section.tsx
+++ src/app/components/image-section.tsx
@@ -1,13 +1,29 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
+const images = ["/mockup1.jpg", "/mockup2.jpg", "/mockup4.jpg"];
 
 export default function ImageSection() {
+  const [contador, setContador] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setContador((prev) => (prev + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(interval); // Limpa o intervalo ao desmontar
+  }, []);
+
   return (
     <section className="w-full min-h-[238px] md:min-h-[600px] relative">
       <Image
-        src={"/mockup.png"}
-        alt="imagem exibindo algumas páginas feitas pela brothers company"
-        className="object-cover"
-        fill
+        src={`/mockup${contador}.jpg`}
+        alt="Imagem de site feito pela brothers company"
+        className="w-full h-auto object-contain"
+        layout="intrinsic"
+        width={1200}
+        height={800}
       />
     </section>
   );
